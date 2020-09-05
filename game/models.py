8be7a7fecbe66c837e37fc8ec407b3fcb2acb239
Ivan Alexandrov <radniforg@yandex.ru>
2020-09-05 19:56:28 +0300

    beta-version

--- game/models.py
+++ game/models.py
@@ -2,12 +2,42 @@ from django.db import models
 
 
 class Player(models.Model):
-    pass
+    cookie = models.CharField(max_length=64,
+                              default='nom')
+
+    def __str__(self):
+        return self.cookie
+
 
 
 class Game(models.Model):
-    pass
+    number = models.IntegerField()
+    ongoing = models.BooleanField(default=True)
+    players = models.ManyToManyField(
+        'Player',
+        through='PlayerGameInfo',
+        related_name='games'
+    )
+
 
 
 class PlayerGameInfo(models.Model):
-    pass
+    player = models.ForeignKey(
+        'Player',
+        verbose_name='Игрок',
+        on_delete=models.CASCADE,
+    )
+    game = models.ForeignKey(
+        'Game',
+        verbose_name='Игра',
+        on_delete=models.CASCADE,
+    )
+    creator = models.BooleanField(verbose_name='Создатель игры',
+                                  default=False)
+    announce = models.BooleanField(verbose_name='Активна ли всё еще игра для этого игрока',
+                                  default=True)
+    victor = models.BooleanField(verbose_name='Победитель',
+                                 default=False)
+
+    class Meta:
+        unique_together = ('player', 'game')
--- game/views.py
+++ game/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from game.forms import GameForm
+from game.models import Game
+
 
 def show_home(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,
+                          'success.html')
+
     return render(
         request,
-        'home.html'
+        'home.html',
+        context = {'form': form}
     )
